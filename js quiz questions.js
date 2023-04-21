ba511975d3c8bfee497ae1ea5cddd31e8dfc3e39
brurou <62688754+brurou@users.noreply.github.com>
2023-04-21 15:32:45 +0200

    Functional

--- js quiz questions.js
+++ js quiz questions.js
@@ -6,7 +6,7 @@ var quizData = [
     image: "",
     description: "What is the starting code?",
     hint: "",
-    task: ""
+    task: "High Five"
   },
   {
     question: " Who wins? By how much? Answer with adam's score-eve's score e.g 10-0",
@@ -14,7 +14,7 @@ var quizData = [
     image: "bread_dance.jpg",
     description: "Adam and Eve play rock-paper-scissors 10 times. You know that: Adam uses rock three times, scissors six times, and paper once. Eve uses rock twice, scissors four times, and paper four times. There are no ties in all 10 games. The order of games is unknown.",
     hint: "Adam",
-    task: ""
+    task: "Jump up and down"
   },
   {
     question: "Find your teams page and enter the code on the back.",
@@ -89,6 +89,7 @@ var quizData = [
   // Add more questions here...
 ];
 
+/*
 // Initialize quiz index
 var quizIndex = 0;
 
@@ -114,7 +115,7 @@ function loadQuestion() {
 
 
     // Generate HTML for current quiz question
-    function generateQuizQuestion() {
+   function generateQuizQuestion() {
       var currentQuestion = quizData[quizIndex];
       var questionHTML = "<p>" + currentQuestion.description + "</p>" +
                          "<p>" + currentQuestion.question + "</p>" +
@@ -122,10 +123,65 @@ function loadQuestion() {
                          "<input type='text' id='answer'>" +
                          "<button class = 'button' onclick='checkAnswer()'>Submit</button>";
       quizContainer.innerHTML = questionHTML;
+    } */
+
+    var currentQuestion = 0;
+var numTries = 0;
+
+var questionElem = document.getElementById("question");
+var answerElem = document.getElementById("answer");
+var submitButton = document.getElementById("submit-answer");
+var resultElem = document.getElementById("result");
+var imageElem = document.getElementById("image");
+var descriptionElem = document.getElementById("description");
+var nextButton = document.getElementById("next-question");
+var task = document.getElementById("task")
+
+function displayQuestion() {
+  var questionObj = quizData[currentQuestion];
+  questionElem.textContent = questionObj.question;
+  imageElem.src = questionObj.image;
+  descriptionElem.textContent = questionObj.description;
+  resultElem.textContent = "";
+  answerElem.value = "";
+  answerElem.focus();
+  submitButton.disabled = false;
+  nextButton.style.display = "none";
+}
+
+function checkAnswer() {
+  var questionObj = quizData[currentQuestion];
+  var userAnswer = answerElem.value.trim().toLowerCase();
+  if (userAnswer === questionObj.answer.toLowerCase()) {
+    resultElem.textContent = questionObj.task;
+    numTries = 0;
+    submitButton.disabled = true;
+    nextButton.style.display = "inline-block";
+  } else {
+    numTries++;
+    resultElem.textContent = "Incorrect! Try again.";
+    if (numTries >= 3) {
+      resultElem.textContent += " Here's a hint: " + questionObj.hint;
     }
+  }
+}
+
+function goToNextQuestion() {
+  currentQuestion++;
+  if (currentQuestion === quizData.length) {
+    alert("You have completed the quiz!");
+  } else {
+    displayQuestion();
+  }
+}
+
+displayQuestion();
+
+submitButton.addEventListener("click", checkAnswer);
+nextButton.addEventListener("click", goToNextQuestion);
 
 
-
+/*
 // Check user's answer against correct answer
 function checkAnswer() {
   var input = document.getElementById("answer").value;
@@ -160,4 +216,4 @@ colorIcons.forEach((icon) => {
     const color = icon.getAttribute('data-color');
     body.style.backgroundColor = color;
   });
-});
+}); */
